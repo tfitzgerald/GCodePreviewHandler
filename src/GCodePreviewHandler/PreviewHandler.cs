ab4a0da201f9200b2fb24bee1a209916c002c98e
Thane H. Fitzgerald <tfitzgerald@rogers.com>
2026-05-06 15:32:34 -0400

    fixed clsid = {D0A1F2C1-ABCD-4F00-9A11-1234567890AB}

--- src/GCodePreviewHandler/PreviewHandler.cs
+++ src/GCodePreviewHandler/PreviewHandler.cs
@@ -1,27 +1,219 @@
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Windows.Forms;
 using GCodePreviewControl;
 
 namespace GCodePreviewHandler
 {
+    // CLSID for the preview handler (keep this stable)
     [ComVisible(true)]
     [Guid("D0A1F2C1-ABCD-4F00-9A11-1234567890AB")]
-    public class PreviewHandler : UserControl
+    [ClassInterface(ClassInterfaceType.None)]
+    public class PreviewHandler :
+        IPreviewHandler,
+        IInitializeWithFile,
+        IOleWindow
     {
-        private readonly GCodePreviewControl.GCodePreviewControl _viewer =
-            new GCodePreviewControl.GCodePreviewControl();
+        private IntPtr _parentHwnd = IntPtr.Zero;
+        private Rectangle _bounds;
+        private bool _isInitialized;
+        private string? _filePath;
 
-        public PreviewHandler()
+        private Form? _hostForm;
+        private ElementHost? _elementHost;
+        private GCodePreviewControl.GCodePreviewControl? _viewer;
+
+        #region IInitializeWithFile
+
+        public void Initialize(string pszFilePath, STGM grfMode)
         {
-            Controls.Add(_viewer);
-            _viewer.Dock = DockStyle.Fill;
+            _filePath = pszFilePath;
+            _isInitialized = true;
         }
 
-        public void Load(string path)
+        #endregion
+
+        #region IPreviewHandler
+
+        public void SetWindow(IntPtr hwnd, ref RECT rect)
         {
-            var text = System.IO.File.ReadAllText(path);
+            _parentHwnd = hwnd;
+            _bounds = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+
+            if (_hostForm == null)
+            {
+                _hostForm = new Form
+                {
+                    FormBorderStyle = FormBorderStyle.None,
+                    TopLevel = false,
+                    ShowInTaskbar = false
+                };
+
+                _elementHost = new ElementHost
+                {
+                    Dock = DockStyle.Fill
+                };
+
+                _viewer = new GCodePreviewControl.GCodePreviewControl();
+                _elementHost.Child = _viewer;
+
+                _hostForm.Controls.Add(_elementHost);
+                _hostForm.CreateControl();
+
+                NativeMethods.SetParent(_hostForm.Handle, _parentHwnd);
+                _hostForm.Bounds = _bounds;
+                _hostForm.Show();
+            }
+            else
+            {
+                _hostForm.Bounds = _bounds;
+            }
+        }
+
+        public void SetRect(ref RECT rect)
+        {
+            _bounds = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            if (_hostForm != null)
+            {
+                _hostForm.Bounds = _bounds;
+            }
+        }
+
+        public void DoPreview()
+        {
+            if (!_isInitialized || string.IsNullOrEmpty(_filePath) || _viewer == null)
+                return;
+
+            string text = File.ReadAllText(_filePath);
             _viewer.LoadGCode(text);
         }
+
+        public void Unload()
+        {
+            _viewer = null;
+
+            if (_elementHost != null)
+            {
+                _elementHost.Dispose();
+                _elementHost = null;
+            }
+
+            if (_hostForm != null)
+            {
+                _hostForm.Close();
+                _hostForm.Dispose();
+                _hostForm = null;
+            }
+
+            _parentHwnd = IntPtr.Zero;
+            _isInitialized = false;
+            _filePath = null;
+        }
+
+        public void SetFocus()
+        {
+            _hostForm?.Focus();
+        }
+
+        public void QueryFocus(out IntPtr phwnd)
+        {
+            phwnd = _hostForm?.Handle ?? IntPtr.Zero;
+        }
+
+        public uint TranslateAccelerator(ref MSG pmsg)
+        {
+            return 0; // no keyboard handling
+        }
+
+        #endregion
+
+        #region IOleWindow
+
+        public void GetWindow(out IntPtr phwnd)
+        {
+            phwnd = _hostForm?.Handle ?? IntPtr.Zero;
+        }
+
+        public void ContextSensitiveHelp(bool fEnterMode)
+        {
+            // not used
+        }
+
+        #endregion
     }
+
+    #region Interop
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MSG
+    {
+        public IntPtr hwnd;
+        public uint message;
+        public IntPtr wParam;
+        public IntPtr lParam;
+        public uint time;
+        public int pt_x;
+        public int pt_y;
+    }
+
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("b7d14566-0509-4cce-a71f-0a554233bd9b")]
+    public interface IInitializeWithFile
+    {
+        void Initialize([MarshalAs(UnmanagedType.LPWStr)] string pszFilePath, STGM grfMode);
+    }
+
+    [Flags]
+    public enum STGM : uint
+    {
+        READ = 0x00000000,
+        WRITE = 0x00000001,
+        READWRITE = 0x00000002
+    }
+
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("8895b1c6-b41f-4c1c-a562-0d564250836f")]
+    public interface IPreviewHandler
+    {
+        void SetWindow(IntPtr hwnd, ref RECT rect);
+        void SetRect(ref RECT rect);
+        void DoPreview();
+        void Unload();
+        void SetFocus();
+        void QueryFocus(out IntPtr phwnd);
+        uint TranslateAccelerator(ref MSG pmsg);
+    }
+
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("00000114-0000-0000-C000-000000000046")]
+    public interface IOleWindow
+    {
+        void GetWindow(out IntPtr phwnd);
+        void ContextSensitiveHelp(bool fEnterMode);
+    }
+
+    internal static class NativeMethods
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+    }
+
+    // WinForms/WPF bridge
+    public class ElementHost : System.Windows.Forms.Integration.ElementHost { }
+
+    #endregion
 }
